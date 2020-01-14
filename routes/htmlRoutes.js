2ade8c37704b829b9f0f2e9205aa759cd6c55761
heckman-codes <liamheckmancode@gmail.com>
2020-01-13 20:37:06 -0800

    project 2 boilerplate code

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+var db = require("../models");
+
+module.exports = function (app) {
+  // Load index page
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: JSON.parse(JSON.stringify(dbExamples))
+      });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: JSON.parse(JSON.stringify(dbExample))
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
+};
