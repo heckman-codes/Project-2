5772752681b269929cb98cfc5db4456b5c1f34bb
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-01-18 13:56:12 -0800

    added basics for authentication

---
+++ config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
+        'user_profile',
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql'
+        }
+    );
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 /* eslint-disable prettier/prettier */
 require("dotenv").config();
 var express = require("express");
+const session = require("express-session");
+
 var exphbs = require("express-handlebars");
+const sequelize = require("./config/config")
 
 var db = require("./models");
 
 var app = express();
+var sess = {
+  secret: "Session Secret Is Secret",
+  cookie: {}
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
 var PORT = process.env.PORT || 3005;
 
 // Middleware
