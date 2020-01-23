12fca535843b17df3ca698892f29f912900e4a85
heckman-codes <liamheckmancode@gmail.com>
2020-01-22 20:53:38 -0800

    working user create post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ const petfinder = require("@petfinder/petfinder-js");
 //   secret: "1f7G1WupvYNO51Jf5vyueYCqvyOb3LjzoP7voWXs"
 // });
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
   // app.get("/api/examples", function (req, res) {
   //   db.Example.findAll({}).then(function (dbExamples) {
@@ -15,15 +15,15 @@ module.exports = function(app) {
   // });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/usercreate", function (req, res) {
+    db.User.create(req.body).then(function (userCreate) {
+      res.json(userCreate);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.json(dbExample);
