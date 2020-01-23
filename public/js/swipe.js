b51ba94b8e475ae80352407a6a0a1bf395007cf3
Kevin-eh <54459190+Kevin-eh@users.noreply.github.com>
2020-01-22 20:27:37 -0800

    Click and drag initial functionality. But it won't return home yet....

--- public/js/swipe.js
+++ public/js/swipe.js
@@ -1,70 +1,69 @@
-// var dragItem = document.querySelector("#pet-card");
-// var dragBox = document.querySelector("#petContainer");
+var dragItem = document.querySelector("#pet-card");
+var dragBox = document.querySelector("#petContainer");
 
-// var active = false;
-// var currentX;
-// var currentY;
-// var initialX;
-// var initialY;
-// var xOffset = 0;
-// var yOffset = 0;
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
 
-// dragBox.addEventListener("touchstart", dragStart, false);
-// dragBox.addEventListener("touchend", dragEnd, false);
-// dragBox.addEventListener("touchmove", drag, false);
+dragBox.addEventListener("touchstart", dragStart, false);
+dragBox.addEventListener("touchend", dragEnd, false);
+dragBox.addEventListener("touchmove", drag, false);
 
-// dragBox.addEventListener("mousedown", dragStart, false);
-// dragBox.addEventListener("mouseup", dragEnd, false);
-// dragBox.addEventListener("mousemove", drag, false);
+dragBox.addEventListener("mousedown", dragStart, false);
+dragBox.addEventListener("mouseup", dragEnd, false);
+dragBox.addEventListener("mousemove", drag, false);
 
-// function dragStart(e) {
-//   if (e.type === "touchstart") {
-//     console.log("lets drag");
-//     console.log(e);
-//     initialX = e.touches[0].clientX - xOffset;
-//     initialY = e.touches[0].clientY - yOffset;
-//   } else {
-//     console.log("life is a drag");
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    console.log("lets drag");
+    console.log(e);
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    console.log("life is a drag");
+    active = true;
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
 
-//     initialX = e.clientX - xOffset;
-//     initialY = e.clientY - yOffset;
-//   }
+  if (e.target === dragItem) {
+    console.log("it's true!!!");
+    active = true;
+  }
+}
 
-//   if (e.target === dragItem) {
-//     console.log("it's true!!!");
-//     active = true;
-//   }
-// }
+function dragEnd(e) {
+  currentX = 0;
+  currentY = 0;
+  active = false;
+}
 
-// function dragEnd(e) {
-//   initialX = currentX;
-//   initialY = currentY;
+function drag(e) {
+  if (active) {
+    e.preventDefault();
+    console.log("lets go");
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
 
-//   active = false;
-// }
+    xOffset = currentX;
+    yOffset = currentY;
 
-// function drag(e) {
-//   if (active) {
-//     e.preventDefault();
-//     console.log("lets go");
-//     if (e.type === "touchmove") {
-//       currentX = e.touches[0].clientX - initialX;
-//       currentY = e.touches[0].clientY - initialY;
-//     } else {
-//       currentX = e.clientX - initialX;
-//       currentY = e.clientY - initialY;
-//     }
+    setTranslate(currentX, currentY, dragItem);
+  }
+}
 
-//     xOffset = currentX;
-//     yOffset = currentY;
-
-//     setTranslate(currentX, currentY, dragItem);
-//   }
-// }
-
-// function setTranslate(xPos, yPos, el) {
-//   el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-// }
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
 
 var container = document.querySelector("#petContainer");
 var listener = SwipeListener(container);
